c50056ea66e388d2eff65be7d5796b0fe27f0b59
David Carter <davezatch@gmail.com>
2013-06-22 00:09:23 +0200

    * got rid of all the canvas stuff, wasn't really working * jQuery for the win! * added buttons to start and reset board * shapes now created and placed randomly * scoring displayed at end of 10 second timer * best score = 0 * random colors!

--- app.js
+++ app.js
@@ -1,227 +1,192 @@
 $(document).ready(function() {
-    init();
-});
+    var $container = $(".container");
+    var scores = {
+        "rotate1": 0,
+        "rotate2": 0,
+        "rotate3": 0,
+        "rotate4": 0
+    }
+  // the same as yours.
+  function rotateOnMouse(e, pw) {
+    // console.log(e.target);
+      var offset = pw.offset();
+      var center_x = (offset.left) + ($(pw).width() / 2);
+      var center_y = (offset.top) + ($(pw).height() / 2);
+      var mouse_x = e.pageX;
+      var mouse_y = e.pageY;
+      var radians = Math.atan2(mouse_x - center_x, mouse_y - center_y);
+      var degree = Math.round((radians * (180 / Math.PI) * -1) + 180);
+      // if (degree < 0) { degree = 360 + degree; }
 
-// Constructor for Shape objects to hold data for all drawn objects.
-// For now they will just be defined as rectangles.
-function Shape(x, y, w, h, fill) {
-  this.x = x || 0;
-  this.y = y || 0;
-  this.w = w || 1;
-  this.h = h || 1;
-  // this.deg = deg || 90;
-  this.fill = fill || '#AAAAAA';
-}
+      var calibratedScore = 0;
 
-// Draws this shape to a given context
-Shape.prototype.draw = function(ctx) {
-  ctx.fillStyle = this.fill;
-  ctx.fillRect(this.x, this.y, this.w, this.h);
-  // ctx.rotate(this.deg);
-  // console.log(this.deg);
-}
+      calibratedScore = -(degree - 360) + 180;
 
-// Determine if a point is inside the shape's bounds
-Shape.prototype.contains = function(mx, my) {
-  // All we have to do is make sure the Mouse X,Y fall in the area between
-  // the shape's X and (X + Height) and its Y and (Y + Height)
-  return  (this.x <= mx) && (this.x + this.w >= mx) &&
-          (this.y <= my) && (this.y + this.h >= my);
-}
+      calibratedScore = (calibratedScore >= 360) ? calibratedScore - 360 : calibratedScore;
 
-function CanvasState(canvas) {
+      console.log(calibratedScore);
 
-  this.canvas = canvas;
-  this.width = canvas.width;
-  this.height = canvas.height;
-  this.ctx = canvas.getContext('2d');
-  // This complicates things a little but but fixes mouse co-ordinate problems
-  // when there's a border or padding. See getMouse for more detail
-  var stylePaddingLeft, stylePaddingTop, styleBorderLeft, styleBorderTop;
-  if (document.defaultView && document.defaultView.getComputedStyle) {
-    this.stylePaddingLeft = parseInt(document.defaultView.getComputedStyle(canvas, null)['paddingLeft'], 10)      || 0;
-    this.stylePaddingTop  = parseInt(document.defaultView.getComputedStyle(canvas, null)['paddingTop'], 10)       || 0;
-    this.styleBorderLeft  = parseInt(document.defaultView.getComputedStyle(canvas, null)['borderLeftWidth'], 10)  || 0;
-    this.styleBorderTop   = parseInt(document.defaultView.getComputedStyle(canvas, null)['borderTopWidth'], 10)   || 0;
+      scores[pw[0].id] = calibratedScore;
+
+      $(pw).css('-moz-transform', 'rotate(' + degree + 'deg)');
+      $(pw).css('-webkit-transform', 'rotate(' + degree + 'deg)');
+      $(pw).css('-o-transform', 'rotate(' + degree + 'deg)');
+      $(pw).css('-ms-transform', 'rotate(' + degree + 'deg)');
+
+      // $("#angle").text(degree);
   }
-  // Some pages have fixed-position bars (like the stumbleupon bar) at the top or left of the page
-  // They will mess up mouse coordinates and this fixes that
-  var html = document.body.parentNode;
-  this.htmlTop = html.offsetTop;
-  this.htmlLeft = html.offsetLeft;
 
-  // **** Keep track of state! ****
-  
-  this.valid = false; // when set to false, the canvas will redraw everything
-  this.shapes = [];  // the collection of things to be drawn
-  this.dragging = false; // Keep track of when we are dragging
-  // the current selected object. In the future we could turn this into an array for multiple selection
-  this.selection = null;
-  this.dragoffx = 0; // See mousedown and mousemove events for explanation
-  this.dragoffy = 0;
-  
-  // **** Then events! ****
-  
-  // This is an example of a closure!
-  // Right here "this" means the CanvasState. But we are making events on the Canvas itself,
-  // and when the events are fired on the canvas the variable "this" is going to mean the canvas!
-  // Since we still want to use this particular CanvasState in the events we have to save a reference to it.
-  // This is our reference!
-  var myState = this;
-  
-  //fixes a problem where double clicking causes text to get selected on the canvas
-  canvas.addEventListener('selectstart', function(e) { e.preventDefault(); return false; }, false);
-  // Up, down, and move are for dragging
-  canvas.addEventListener('mousedown', function(e) {
-    var mouse = myState.getMouse(e);
-    var mx = mouse.x;
-    var my = mouse.y;
-    var shapes = myState.shapes;
-    var l = shapes.length;
-    for (var i = l-1; i >= 0; i--) {
-      if (shapes[i].contains(mx, my)) {
-        var mySel = shapes[i];
-        // Keep track of where in the object we clicked
-        // so we can move it smoothly (see mousemove)
-        myState.dragoffx = mx - mySel.x;
-        myState.dragoffy = my - mySel.y;
-        myState.dragging = true;
-        myState.selection = mySel;
-        myState.valid = false;
+  function handleMouse() {
+      $('.rotateme').mousedown(function(e) {
+        e.preventDefault();
+          var target = $(e.originalEvent.target);
+        $(document).bind('mousemove.rotate', function(e2) {
+          // console.log($('.rotateme'));
+          if (target.hasClass("rotateme")) {
+            rotateOnMouse(e2, target);
+            
+          }
+        });
+      });
+  }
 
-        myState.clickAngle = myState.getAngle( myState.dragoffx + myState.selection.offsetLeft, myState.dragoffy + myState.selection.offsetTop, event.clientX, event.clientY  ) - myState.angle;
-
-        return;
+  function reportScore() {
+    var finalScore = 0;
+    $.each(scores, function(key, value) {
+      // tweak score because we're actually centering at 180, not 0 degrees
+      if (value >= 180) {
+        finalScore = finalScore + value - 180;
+      } else {
+        finalScore = finalScore + 180 - value;
       }
-    }
-    // havent returned means we have failed to select anything.
-    // If there was an object selected, we deselect it
-    if (myState.selection) {
-      myState.selection = null;
-      myState.valid = false; // Need to clear the old selection border
-    }
-  }, true);
-  canvas.addEventListener('mousemove', function(e) {
-    if (myState.dragging){
-      var mouse = myState.getMouse(e);
-      // We don't want to drag the object by its top-left corner, we want to drag it
-      // from where we clicked. Thats why we saved the offset and use it here
-      myState.selection.x = mouse.x - myState.dragoffx;
-      myState.selection.y = mouse.y - myState.dragoffy;   
-
-      myState.angle =  ( myState.getAngle( myState.dragoffx + myState.selection.offsetLeft, myState.dragoffy + myState.selection.offsetTop, event.clientX, event.clientY  ) - myState.clickAngle);
-
-      myState.draw(myState.angle);
-
-      myState.valid = true; // Something's dragging so we must redraw
-    }
-  }, true);
-  canvas.addEventListener('mouseup', function(e) {
-    myState.dragging = false;
-  }, true);
-  // double click for making new shapes
-  // canvas.addEventListener('dblclick', function(e) {
-  //   var mouse = myState.getMouse(e);
-  //   myState.addShape(new Shape(mouse.x - 10, mouse.y - 10, 20, 20, 'rgba(0,255,0,.6)'));
-  // }, true);
-  
-  // **** Options! ****
-  
-  this.selectionColor = '#CC0000';
-  this.selectionWidth = 2;  
-  this.interval = 30;
-  setInterval(function() { myState.draw(); }, myState.interval);
-}
-
-CanvasState.prototype.addShape = function(shape) {
-  this.shapes.push(shape);
-  this.valid = false;
-}
-
-CanvasState.prototype.clear = function() {
-  this.ctx.clearRect(0, 0, this.width, this.height);
-}
-
-CanvasState.prototype.getAngle = function( cX, cY, mX, mY ) {
-    var angle = Math.atan2(mY - cY, mX - cX);
-    return angle;
-}
-
-// While draw is called as often as the INTERVAL variable demands,
-// It only ever does something if the canvas gets invalidated by our code
-CanvasState.prototype.draw = function() {
-
-  // if our state is invalid, redraw and validate!
-  if (!this.valid) {
-    var ctx = this.ctx;
-    var shapes = this.shapes;
-    this.clear();
-    
-    // ** Add stuff you want drawn in the background all the time here **
-    
-    // draw all shapes
-    var l = shapes.length;
-    for (var i = 0; i < l; i++) {
-      var shape = shapes[i];
-      // We can skip the drawing of elements that have moved off the screen:
-      if (shape.x > this.width || shape.y > this.height ||
-          shape.x + shape.w < 0 || shape.y + shape.h < 0) continue;
-      shapes[i].draw(ctx);
-    }
-    
-    // draw selection
-    // right now this is just a stroke along the edge of the selected Shape
-    if (this.selection != null) {
-      ctx.strokeStyle = this.selectionColor;
-      ctx.lineWidth = this.selectionWidth;
-      var mySel = this.selection;
-      ctx.strokeRect(mySel.x,mySel.y,mySel.w,mySel.h);
-    }
-    
-    // ** Add stuff you want drawn on top all the time here **
-    
-    this.valid = true;
-  }
-}
-
-
-// Creates an object with x and y defined, set to the mouse position relative to the state's canvas
-// If you wanna be super-correct this can be tricky, we have to worry about padding and borders
-CanvasState.prototype.getMouse = function(e) {
-  var element = this.canvas, offsetX = 0, offsetY = 0, mx, my;
-  
-  // Compute the total offset
-  if (element.offsetParent !== undefined) {
-    do {
-      offsetX += element.offsetLeft;
-      offsetY += element.offsetTop;
-    } while ((element = element.offsetParent));
+    });
+    finalScore = finalScore - 720;
+    $("#score").text(finalScore);
+    // return finalScore;
   }
 
-  // Add padding and border style widths to offset
-  // Also add the <html> offsets in case there's a position:fixed bar
-  offsetX += this.stylePaddingLeft + this.styleBorderLeft + this.htmlLeft;
-  offsetY += this.stylePaddingTop + this.styleBorderTop + this.htmlTop;
+  $(".score").click(function(e) {
+    e.preventDefault();
+    reportScore();
+  });
 
-  mx = e.pageX - offsetX;
-  my = e.pageY - offsetY;
-  
-  // We return a simple javascript object (a hash) with x and y defined
-  return {x: mx, y: my};
-}
+  function startGame() {
+    handleMouse();
+    setTimeout(function() {
+        $(document).unbind('mousemove.rotate');
+        $(".rotateme").unbind('mousedown');
+        reportScore()
+    }, 10000);
+        var count=10;
 
-// If you dont want to use <body onLoad='init()'>
-// You could uncomment this init() reference and place the script reference inside the body tag
-//init();
+        var counter=setInterval(timer, 1000); //1000 will  run it every 1 second
 
-function init() {
-  var s = new CanvasState(document.getElementById('stack'));
-  s.addShape(new Shape(225,225,150,50, 'lightskyblue')); // The default is gray
-  // s.addShape(new Shape(60,140,40,60, 'lightskyblue'));
-  // // Lets make some partially transparent
-  // s.addShape(new Shape(80,150,60,30, 'rgba(127, 255, 212, .5)'));
-  // s.addShape(new Shape(125,80,30,80, 'rgba(245, 222, 179, .7)'));
-}
+        function timer()
+        {
+          count=count-1;
+          if (count <= 0)
+          {
+             clearInterval(counter);
+             return;
+          }
 
-// Now go make something amazing!
+          $("#timer").text(count);
+        }
+    }
 
+  $(".start").click(function(e) {
+    e.preventDefault();
+    startGame();
+  });
+
+  // $(".start").click();
+
+  $(document).mouseup(function(e) {
+    $(document).unbind('mousemove.rotate');
+  });
+
+  function randomColor() {
+    var color = 'rgb(' + Math.floor(Math.random()*255) + ',' +
+                            Math.floor(Math.random()*255) + ',' + Math.floor(Math.random()*255) + ')';
+    color = toHex(color);
+    return "#" + color;
+  }
+
+  function componentToHex(c) {
+      var hex = c.toString(16);
+      return hex.length == 1 ? "0" + hex : hex;
+  }
+
+  function rgbToHex(rgbArr) {
+    var result = "";
+    for (var i = 0; i < rgbArr.length; i++) {
+        result += componentToHex(parseInt(rgbArr[i]));
+    }
+    return result;
+  }
+
+  function toHex(rgb) {
+    rgb = rgb.slice(4, -1);
+    rgb = rgb.split(",");
+    return rgbToHex(rgb);
+  }
+
+  // var shapeOffset = 40;
+
+  function createShape(shapeOffsetX, shapeOffsetY) {
+    var shape = $("<div class='rotateme'></div>");
+
+    var randomWidth = Math.floor(Math.random() * 50) + 50;
+    var randomHeight = randomWidth + Math.floor(Math.random() * 3) + 50;
+
+    var rotateAmt = Math.floor(Math.random() * 300) + 60;
+
+    shape.css({
+      width: randomWidth,
+      height: randomHeight,
+      "background-color": randomColor(),
+
+      "left": function() {
+        return Math.floor(Math.random()*100) + shapeOffsetX;
+      },
+
+      "top": function() {
+        return Math.floor(Math.random()*100) + shapeOffsetY;
+      },
+
+      '-moz-transform': 'rotate(' + rotateAmt + 'deg)',
+      '-webkit-transform': 'rotate(' + rotateAmt + 'deg)',
+      '-o-transform': 'rotate(' + rotateAmt + 'deg)',
+      '-ms-transform': 'rotate(' + rotateAmt + 'deg)'
+      // "transform": "rotate(" + rotateAmt + ")"
+    });
+
+    var zoneBox = $("<div class='zone'></div>");
+
+    zoneBox.css({
+      width: shape.width() + 6,
+      height: shape.height() + 6,
+      left: parseInt(shape.css("left")) - 3,
+      top: parseInt(shape.css("top")) - 3
+    });
+
+    $container.append(zoneBox);
+
+    $container.append(shape);
+  }
+
+  $(".reset").click(function() {
+    createBoard();
+  });
+
+  function createBoard() {
+    $container.empty();
+    createShape(50, 50);
+    createShape(50, 300);
+    createShape(300, 50);
+    createShape(300, 300);
+  }
+
+  createBoard();
+
+});
